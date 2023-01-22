e330a35d95559d330f4ecc36d7205d81a3aa8530
Paul <mothcar@gmail.com>
2023-01-22 19:53:29 +0000

    color chage

--- src/components/MuiBottomNavigation.jsx
+++ src/components/MuiBottomNavigation.jsx
@@ -10,16 +10,16 @@ const MuiBottomNavigation = () => {
   const navigate = useNavigate();
   return (
     <BottomNavigation
-      sx={{ width: "100%", position: "absolute", bottom: 0 }}
+      sx={{ width: "100%", position: "absolute", bottom: 0   }}
       value={value}
       onChange={(e, newValue) => {
         setValue(newValue);
       }}
       showLabels
     >
-      <BottomNavigationAction label="Home" icon={<HomeIcon />} onClick={()=>navigate('/')} />
-      <BottomNavigationAction label="Favorite" icon={<FavoriteIcon />} onClick={()=>navigate('/favorite')}/>
-      <BottomNavigationAction label="Person" icon={<PersonIcon />} onClick={()=>navigate('/profile')}/>
+      <BottomNavigationAction sx={{"&.Mui-selected":{color:'red'} }} label="Home" icon={<HomeIcon />} onClick={()=>navigate('/')} />
+      <BottomNavigationAction sx={{"&.Mui-selected":{color:'red'} }} label="Favorite" icon={<FavoriteIcon />} onClick={()=>navigate('/favorite')}/>
+      <BottomNavigationAction sx={{"&.Mui-selected":{color:'red'} }} label="Person" icon={<PersonIcon />} onClick={()=>navigate('/profile')}/>
     </BottomNavigation>
   );
 };
